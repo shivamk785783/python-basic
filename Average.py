84d7e9ee1cba794c02fec3d36979514e3e6dd39a
shivamk785783 <shivamk785783@gmail.com>
2026-03-08 18:15:27 +0530

    Add files via upload

---
+++ Average.py
@@ -0,0 +1,5 @@
+a = int(input("Enter first integer: "))
+b = int(input("Enter second integer: "))
+c = float(input("Enter one float: "))
+average = (float(a) + float(b) + float(c)) / 3
+print("Average =", average)
