e3becfdd314c9e8dc7105ac566e3cbba51a87244
shivamk785783 <shivamk785783@gmail.com>
2026-03-08 18:16:14 +0530

    Add files via upload

---
+++ Prime.py
@@ -0,0 +1,12 @@
+def is_prime(n):
+    if n<2:
+        return False
+        for i in range(2,n):
+                   if n%i==0:
+                     return False
+    return True
+num=int(input("Enter a No.: "))
+if is_prime(num):
+   print("prime number")
+else:
+   print("Not a prime number")
